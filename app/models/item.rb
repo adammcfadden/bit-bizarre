5c92fe701b3f05f2b6384df09b8fd446ef4915d5
Ian MacDonald & Alec Arme <student@epicodus.com>
2015-07-22 14:18:48 -0700

    Add most_recent to Item

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,8 @@ class Item < ActiveRecord::Base
 
   validates :name, :body, :price, presence: true
   belongs_to :user
+
+  def self.most_recent
+    order(created_at: :desc)[0..5]
+  end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -5,4 +5,13 @@ RSpec.describe Item, type: :model do
   it { should validate_presence_of :name }
   it { should validate_presence_of :body }
   it { should validate_presence_of :price }
+
+  describe ".most_recent" do
+    it "will return the 6 most recently added items" do
+      20.times do
+        FactoryGirl.create(:item, name: Faker::Commerce.product_name, body: Faker::Lorem.paragraph(2), price: Faker::Commerce.price)
+      end
+      expect(Item.most_recent.length).to eq 6
+    end
+  end
 end
