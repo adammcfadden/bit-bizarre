69ddf04b4e502cd0bed1551bb20e0caeb9627322
Toby Alden & Toby Alden <student@epicodus.com>
2015-07-23 10:32:47 -0700

    add paperclip to items

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,9 @@
 class Item < ActiveRecord::Base
 
+  has_attached_file :avatar, styles: { medium: "300x300>", fit: "100x100>" }
+
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+
   validates :name, :body, :price, presence: true
   belongs_to :user
 
